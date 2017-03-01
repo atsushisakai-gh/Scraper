154cdea6c917063e3941e214e66aa2767ad8c8ab
Atsushi Sakai <sakai.atsushi@gmail.com>
2017-03-02 00:37:15 +0900

    Kitura build

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Scraper"
+    name: "Scraper",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6)
+    ]
 )
