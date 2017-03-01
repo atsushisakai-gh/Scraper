f28a5865c21789ee177d83d00c24c19c37e4ced2
Atsushi Sakai <sakai.atsushi@gmail.com>
2017-03-02 00:53:10 +0900

    build setting PostgreSQL

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Scraper",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6)
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0),
     ]
 )
