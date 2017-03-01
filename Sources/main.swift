9229824120141ebcb122ffed2a2bd714844099c5
Atsushi Sakai <sakai.atsushi@gmail.com>
2017-03-02 01:05:46 +0900

    Postgreからのデータフェッチ簡単なコード

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,17 @@
+import Foundation
 import Kitura
 import HeliumLogger
+import PostgreSQL
 
 HeliumLogger.use()
+
+let host = "localhost"
+let port = Int32(5432)
+let database = "tryswift"
+let username = "radioboo"
+let password = "qwe12345"
+
+var postgreConnection: PostgreSQL.Connection!
 
 // Create a new router
 let router = Router()
@@ -9,7 +19,25 @@ let router = Router()
 // Handle HTTP GET requests to /
 router.get("/") {
     request, response, next in
-    response.send("Hello, World!")
+
+    let connectionString = URL(string: "postgres://\(username):\(password)@\(host):\(port)/\(database)")!
+    postgreConnection = try PostgreSQL.Connection(info: .init(connectionString))
+    
+    try postgreConnection.open()
+    
+    guard postgreConnection.internalStatus == PostgreSQL.Connection.InternalStatus.OK else {
+        return
+    }
+    
+    let query = "SELECT * FROM blogs;"
+    let result = try postgreConnection.execute(query)
+    
+    var url: String? = nil
+    for i in 0 ..< result.count {
+        url = try String(describing: result[i].data("url"))
+    }
+
+    response.send(url!)
     next()
 }
 
